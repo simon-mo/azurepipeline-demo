083d767ad4cc50673ae44aed6e52dbe58878c676
Simon Mo <xmo@berkeley.edu>
2019-07-24 00:49:53 -0700

    Check environ; update tests

--- test_complex.py
+++ test_complex.py
@@ -5,6 +5,6 @@ import itertools
     "arg1, arg2",
     [(i,j) for i, j in itertools.product(list(range(50)),repeat=2)])
 def test_param(arg1, arg2):
-    if arg1 + arg2 == 53:
+    if arg1 + arg2 == 64:
         assert False
     assert True

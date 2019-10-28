abf1b7d67775eac3e66cc6a1d9ef7145e0d9bfc6
Simon Mo <xmo@berkeley.edu>
2019-10-28 16:18:28 -0700

    Fix tests

--- test_complex.py
+++ test_complex.py
@@ -3,8 +3,12 @@ import itertools
 
 @pytest.mark.parametrize(
     "arg1, arg2",
-    [(i,j) for i, j in itertools.product(list(range(50)),repeat=2)])
+    [(i,j) for i, j in itertools.product(list(range(3)),repeat=2)])
 def test_param(arg1, arg2):
-    if arg1 + arg2 == 64:
+    if arg1 + arg2 == 2:
         assert False
     assert True
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_skil():
+    pass
